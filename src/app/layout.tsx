4bc5ad23f54d06f4c744dfe59fed16fa401cc2d9
Mikkel Bentsen <bentsen405@gmail.com>
2023-12-04 14:37:23 +0100

    yeye

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,37 @@
+import { Metadata } from "next";
 import "../styles/globals.css";
 import Navbar from "./Navbar";
 import Footer from "./footer";
+import { Lato } from "next/font/google";
+
+const lato = Lato({ weight: ["100", "300", "400", "700", "900"] });
+
+export const metadata: Metadata = {
+  title: "Mikkel Bengtsen",
+  description: "My personal website",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html>
+    <html lang="en">
       <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
-        <div className={"sticky top-0 z-20"}>
-          <Navbar />
-        </div>
-        {children}
-        <div className="mt-10">
-          <Footer />
-        </div>
+        <main className={lato.className}>
+          <div className={"sticky top-0 z-20"}>
+            <Navbar />
+          </div>
+          {children}
+          <div className="mt-10">
+            <Footer />
+          </div>
+        </main>
       </body>
     </html>
   );
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -19,47 +19,45 @@ const Navbar = () => {
   });
 
   return (
-    <>
-      <nav
-        className={
-          "w-full h-20 absolute border-b " +
-          (scroll
-            ? "border-gray-500 backdrop-blur-md duration-300 transition-all"
-            : "border-transparent")
-        }
-      >
-        <div className={"flex flex-row h-full"}>
-          <div
-            className={
-              "flex justify-between items-center text-white h-full w-full px-10"
-            }
-          >
-            <div className={"block"}>
-              <Link href={"/"}>
-                <h1 className={"text-3xl"}>Mikkel</h1>
-              </Link>
-            </div>
-            <div className={"mx-5"}>
-              <ul className={"flex flex-row gap-10 text-lg font-medium"}>
-                <Link href={"#"}>
-                  <li>{"// Home"}</li>
-                </Link>
-                <a href="/#expertise">
-                  <li>{"// Expertise"}</li>
-                </a>
-                <a href={"/#projects"}>
-                  <li>{"// Projects"}</li>
-                </a>
-                <Link href={"/contact"}>
-                  <li>{"// Contact"}</li>
-                </Link>
-              </ul>
-            </div>
-            <div className={""}></div>
+    <nav
+      className={
+        "w-full h-20 absolute border-b " +
+        (scroll
+          ? "border-gray-500 backdrop-blur-md duration-300 transition-all"
+          : "border-transparent")
+      }
+    >
+      <div className={"flex flex-row h-full"}>
+        <div
+          className={
+            "flex justify-between items-center text-white h-full w-full px-10"
+          }
+        >
+          <div className={"block"}>
+            <Link href={"/"}>
+              <h1 className={"text-3xl"}>Mikkel</h1>
+            </Link>
           </div>
+          <div className={"mx-5"}>
+            <ul className={"flex flex-row gap-10 text-lg font-medium"}>
+              <Link href={"#"}>
+                <li>{"// Home"}</li>
+              </Link>
+              <a href="/#expertise">
+                <li>{"// Expertise"}</li>
+              </a>
+              <a href={"/#projects"}>
+                <li>{"// Projects"}</li>
+              </a>
+              <Link href={"/contact"}>
+                <li>{"// Contact"}</li>
+              </Link>
+            </ul>
+          </div>
+          <div className={""}></div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
